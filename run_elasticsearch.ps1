57cc5f60d8f5bbf0a8a21d84a48a74c7d8f57c60
kolcz <17905157+kolcz@users.noreply.github.com>
2025-03-23 12:54:52 +0100

    Started Chapter 17: Deployment on Linux

---
+++ run_elasticsearch.ps1
@@ -0,0 +1,4 @@
+docker run --name elasticsearch -d --rm -p 9200:9200 `
+--memory="2GB" `
+-e discovery.type=single-node -e xpack.security.enabled=false `
+-t docker.elastic.co/elasticsearch/elasticsearch:8.11.1
