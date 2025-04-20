78b4e9c1bb980be10dbb160e62fdce893cd8ac5c
kolcz <17905157+kolcz@users.noreply.github.com>
2025-04-20 23:44:24 +0200

    Fix get default option for DATABASE_URL and remove unneccesary adding index

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
