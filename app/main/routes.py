b7881cdd553010cc5eac8a21e386375631854b96
kolcz <17905157+kolcz@users.noreply.github.com>
2025-05-17 16:19:41 +0200

    Started Chapter 21: User Notifications

--- app/main/routes.py
+++ app/main/routes.py
@@ -84,6 +84,12 @@ def user(username):
     return render_template('user.html', user=user, posts=posts.items,
                            next_url=next_url, prev_url=prev_url, form=form)
 
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    form = EmptyForm()
+    return render_template('user_popup.html', user=user, form=form)
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
