c4830cf2613cbd727cfe08cd30ee5f7efc7dae44
kolcz <17905157+kolcz@users.noreply.github.com>
2025-05-19 12:06:52 -0500

    Started Export Functionality in the Application on Chapter 22

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,9 @@ from config import Config
 from elasticsearch import Elasticsearch
 # from opensearchpy import OpenSearch
 
+from redis import Redis
+import rq
+
 def get_locale():
     return request.accept_languages.best_match(current_app.config['LANGUAGES'])
 
@@ -40,6 +43,9 @@ def create_app(config_class=Config):
     #     if app.config['ELASTICSEARCH_URL'] else None
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
+    
+    app.redis = Redis.from_url(app.config['REDIS_URL'])
+    app.task_queue = rq.Queue('microblog_tasks', connection=app.redis)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
